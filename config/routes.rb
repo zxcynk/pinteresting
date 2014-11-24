9c1b16fb02c49343d334f51cb584eb054152687f
Zuo Xiao <zuoxiaoalex@gmail.com>
2014-11-24 23:46:13 +0800

    add product page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get "about" => "pages#about"
+  get "product" => "pages#product"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
